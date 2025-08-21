b602d3f68a3504f58825b0a71a4fe900928548e1
chupa136 <damir-toktosunov@mail.ru>
2025-08-21 19:01:25 +0000

    HW:DId hw 6

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from posts.models import Post
+from posts.models import Post, Category, Tag
 
 class PostForm(forms.Form):
     image = forms.ImageField(required=False)
@@ -20,3 +20,21 @@ class PostForm(forms.Form):
         if title and content and (title.lower() == content.lower()):
             raise forms.ValidationError("Title and content should be different")
         return cleaned_data
+    
+class SearchForm(forms.Form):
+    q = forms.CharField(max_length=100, required=False, label="Search")
+    category_id = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
+    tag_ids = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False)
+    orderings = (
+        ("created", "По дате"),
+        ("-created", "По дате (по убыванию)"),
+        ("updated", "По обновления"),
+        ("-updated", "По обновления (по убыванию)"),
+        ("title", "По названию"),
+        ("-title", "По названию (по убыванию)"),
+        ("rate", "По рейтенгу"),
+        ("-rate", "По рейтингу (по убыванию)"),
+        (None, None)
+    )
+
+    ordering = forms.ChoiceField(choices=orderings, required=False)
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from posts.models import Post
-from posts.forms import PostForm
+from posts.forms import PostForm, SearchForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 def home_view(request):
     return render(request, "home.html")
@@ -14,8 +15,34 @@ def html_view(request):
 
 @login_required(login_url="login")
 def post_list_view(request):
-    posts = Post.objects.all()
-    return render(request,"posts/post_list.html", context={"posts": posts})
+    limit = 3
+    if request.method == "GET":
+        posts = Post.objects.all()
+        form = SearchForm()
+        q = request.GET.get("q")
+        category_id_value = request.GET.get("category_id")
+        tag_ids = request.GET.get("tag_ids")
+        ordering = request.GET.get("ordering")
+        page = int(request.GET.get("page", 1))
+        if q:
+            posts = posts.filter(Q(title__icontains = q | Q(content__icontains = q)))
+        if category_id_value:
+            posts = posts.filter(category_id=category_id_value)
+        if tag_ids:
+            posts = posts.filter(tags__id__in=tag_ids).distinct()
+        if ordering:
+            posts = posts.order_by(ordering)
+        if page:
+            max_pages = len(posts) / limit
+            if round(max_pages) < max_pages:
+                max_pages = round(max_pages) + 1
+            else:
+                max_pages = round(max_pages)
+            start = (page - 1 ) * limit
+            end = page * limit 
+            posts = posts[start:end]
+
+        return render(request,"posts/post_list.html", context={"posts": posts, "form": form, "max_pages":range(1,  max_pages + 1)})
 
 @login_required(login_url="login")
 def post_detail_view(request, post_id):
@@ -37,3 +64,4 @@ def post_create_view(request):
             image = form.cleaned_data.get("image")
             Post.objects.create(title=title,content=content,image=image)
         return redirect("/posts")
+    
